ee243fd7f51ca2197903237b3ede0d6ad3fe4052
Austin Schenk <cirrustype@icloud.com>
2020-03-30 18:19:48 -0700

    animating ball

--- pong.py
+++ pong.py
@@ -5,7 +5,7 @@ import turtle #comes with python?
 
 wn = turtle.Screen() #creates screen
 wn.title('PyPong')
-wn.bgcolor('red') #sets background color 
+wn.bgcolor('blue') #sets background color 
 wn.setup(width = 800, height = 600)
 
 #stops the window from automaticaly updatingl, lets the game run
@@ -16,7 +16,7 @@ wn.tracer(0)
 pada = turtle.Turtle()
 pada.speed(0) #sets speed of the paddle, 0 is max
 pada.shape('square') #20x20
-pada.color('black')
+pada.color('white')
 pada.shapesize(stretch_wid=5, stretch_len=1)
 pada.penup() #stops the turtle from drawing a line w/ movment
 pada.goto(-350, 0)
@@ -25,7 +25,7 @@ pada.goto(-350, 0)
 padb = turtle.Turtle()
 padb.speed(0) #sets speed of the paddle, 0 is max
 padb.shape('square') #20x20
-padb.color('black')
+padb.color('white')
 padb.shapesize(stretch_wid=5, stretch_len=1)
 padb.penup() #stops the turtle from drawing a line w/ movment
 padb.goto(350, 0)
@@ -36,9 +36,12 @@ padb.goto(350, 0)
 ball = turtle.Turtle()
 ball.speed(0) #sets speed of the paddle, 0 is max
 ball.shape('square') #20x20
-ball.color('black')
+ball.color('white')
 ball.penup() #stops the turtle from drawing a line w/ movment
 ball.goto(0, 0)
+ball.dx = .5 # horizontal movment by 2 pix
+ball.dy = .5 # vertical movment by 2 pix
+# to move the ball put in main loop
 
 
 #animating the paddles 
@@ -78,16 +81,40 @@ wn.onkeypress(padb_up, "Up")
 wn.onkeypress(padb_down, "Down")
 
 
-
-#animating the ball
-
-    
-
-
-    
-    
-    
-
 #main game loop
 while True: 
     wn.update()
+
+
+    
+    #move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+    
+    #border checking
+    # height 600, top y is 300
+    if ball.ycor() > 290:
+        ball.sety(290) #stops at border
+        ball.dy *= -1 # reverses direction of the ball.
+        
+        
+        
+    if ball.ycor() < -290:
+        ball.sety(-290) #stops at border
+        ball.dy *= -1 # reverses direction of the ball.
+
+
+#hitting right side
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+
+
+#hitting left side
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx *= -1
+
+    
+    
+    
